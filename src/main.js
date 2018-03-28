b859d89cb29ca3dfe5071c91983a4e5e826d25e2
Minecrell <minecrell@minecrell.net>
2018-03-28 20:44:42 +0200

    main: Fix changelog collapse button

--- src/main.js
+++ src/main.js
@@ -11,3 +11,11 @@ new Vue({
     router,
     render: h => h(App)
 }).$mount('#app');
+
+// TODO: Replace
+// Based on: http://stackoverflow.com/a/12809794
+/* eslint-disable no-undef */
+$('body').on('click.collapse-next.data-api', '[data-toggle=collapse-next]', function() {
+    const $target = $(this).parent().children('.collapse');
+    $target.data('bs.collapse') ? $target.collapse('toggle') : $target.collapse()
+});
