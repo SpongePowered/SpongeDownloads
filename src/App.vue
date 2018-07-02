42bb9a009b30141633e78100a2ca947843ccffb7
Minecrell <minecrell@minecrell.net>
2018-07-02 18:58:02 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,32 @@
   <div id="app">
     <navbar/>
     <router-view/>
+    <the-footer/>
   </div>
 </template>
 
 <script>
   import Navbar from './components/Navbar.vue'
+  import Footer from './components/Footer.vue'
 
   export default {
     components: {
-      Navbar
+      Navbar, 'the-footer': Footer
     }
   }
 </script>
+
+<style lang="scss">
+  html, body, #app {
+    height: 100%;
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+  }
+
+  main {
+    flex: 1 0 auto;
+  }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <footer>
+    <b-container>
+      <p>Copyright &copy; SpongePowered 2014-2018</p>
+      <p id="fastly">Accelerated by<br>
+        <a href="https://www.fastly.com">
+          <img src="https://www.spongepowered.org/assets/img/fastly.png" alt="Fastly" height="45" width="100">
+        </a>
+      </p>
+    </b-container>
+  </footer>
+</template>
+
+<script>
+  import bContainer from 'bootstrap-vue/es/components/layout/container';
+
+  export default {
+    name: 'the-footer',
+    components: {
+      bContainer
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "../assets/variables";
+
+  footer {
+    background: $sponge_dark_grey;
+    color: white;
+    text-align: center;
+    font-size: 75%;
+
+    padding: 10px;
+
+    #fastly {
+      margin: 0;
+    }
+  }
+</style>
