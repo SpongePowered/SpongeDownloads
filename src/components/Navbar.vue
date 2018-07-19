06572156cf85ccafdae297255c391e3dca8c757f
Jadon Fowler <jadonflower@gmail.com>
2018-07-18 17:15:39 -0700

    Fix styling issues for Ore navbar link
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,7 +23,7 @@
             <li><a href="https://forums.spongepowered.org/c/plugins/plugin-releases">
               <font-awesome-icon icon="plug" fixed-width/> Plugins</a></li>
             <li><a href="https://ore.spongepowered.org/">
-               <img src="https://spongepowered.org/assets/img/icons/ore.svg" alt="" style="height: 16px"> Ore <sup>Beta</sup></a></li>
+               <img src="https://spongepowered.org/assets/img/icons/ore.svg" alt="" class="ore-nav"> Ore <sup>Beta</sup></a></li>
             <li class="active"><a :href="baseUrl">
               <font-awesome-icon icon="download" fixed-width/> Downloads</a></li>
             <li><a href="https://www.spongepowered.org/chat">
