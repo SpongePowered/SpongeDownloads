467ecfc12dd6a93bb78ffda4277f1a0061c7ec26
Daniel Naylor <git@drnaylor.co.uk>
2021-12-01 20:32:01 +0000

    Add maven snapshot sorting, because that could end up being a thing...

--- src/versort.js
+++ src/versort.js
@@ -3,9 +3,12 @@ const VersionSort = (function() {
     // Group 1 is major
     // Group 2 is minor
     // Group 4 is patch
-    // Group 6 is pre/rc
-    // Group 7 is pre number
-    const SEM_VER_REGEX = /^(\d+)\.(\d+)(\.(\d+))?(-(pre|rc)(\d+))?$/i;
+    // Group 7 is pre/rc
+    // Group 8 is pre number
+    // Group 10 is snapshot date
+    // Group 11 is snapshot time
+    // Group 12 is snapshot number
+    const SEM_VER_REGEX = /^(\d+)\.(\d+)(\.(\d+))?(-((pre|rc)(\d+))|((\d+)\.(\d+)-(\d+)))?$/i;
 
     const intOrZero = function(val) {
         if (!val) {
@@ -20,7 +23,7 @@ const VersionSort = (function() {
         } else if (val?.toLowerCase() === "rc") {
             return -1;
         }
-        return 0;
+        return val ? -3 : 0; // if a snapshot, we want to force it lower than anything else.
     }
 
     // [major, minor, patch, pre|rc, version] -- pre = -2, rc = -1
@@ -31,7 +34,7 @@ const VersionSort = (function() {
             return [0, 0, 0, 0, 0];
         }
 
-        return [intOrZero(regexResult[1]), intOrZero(regexResult[2]), intOrZero(regexResult[4]), valueFromPreString(regexResult[6]), intOrZero(regexResult[7])]
+        return [intOrZero(regexResult[1]), intOrZero(regexResult[2]), intOrZero(regexResult[4]), valueFromPreString(regexResult[7] || regexResult[10]), intOrZero(regexResult[8] || regexResult[10]?.concat(regexResult[11].concat(regexResult[12])))]
     }
 
     const sortFunc = function(first, second) {
