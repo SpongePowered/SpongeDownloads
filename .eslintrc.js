f84758a1d8db83aa0fd59c755908b453f750e680
Felix Kleinekathoefer <felix@felixoi.com>
2024-05-14 23:26:56 +0200

    Fix build

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-unused-vars': ["error", { "varsIgnorePattern": "[iI]gnored" }]
+    'no-unused-vars': ["error", { "varsIgnorePattern": "[iI]gnored" }],
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
